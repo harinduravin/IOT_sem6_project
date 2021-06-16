0db9a1d8ad6162fdad7751bd69177db4d647523a
Dhanuka Marasinghe <dhanukamarasinghe1995@gmail.com>
2021-06-16 14:39:02 +0530

    Added node mcu code

---
+++ Node_mcu/Node_mcu_1.ino
@@ -0,0 +1,188 @@
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>          
+#else
+#include <WiFi.h>          
+#endif
+
+#if defined(ESP8266)
+#include <ESP8266WebServer.h>
+#else
+#include <WebServer.h>
+#endif
+
+#include <WiFiManager.h> 
+#include <DNSServer.h>
+#include <PubSubClient.h>
+
+int BUILTIN_LED = 2;
+WebServer server(80);
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient); 
+const char* mqttServer = "test.mosquitto.org";
+
+void setup_wifi() {
+  // Connecting to a WiFi network
+  delay(5000);
+  WiFiManager wifiManager; 
+  wifiManager.autoConnect("IoT6B_G05");
+}
+
+void setupMQTT() {
+  client.setServer(mqttServer,1883);
+//  client.setCallback(callback);
+  }
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("IOT_6B/G05/start", "Hello World");
+      // ... and resubscribe
+//      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  WiFi.mode(WIFI_AP_STA);
+  Serial.begin(115200);
+  setup_wifi();
+  setupMQTT();
+  
+  server.on("/", handle_OnConnect);
+  server.on("/USD", handle_USD);
+  server.on("/JPY", handle_JPY);
+  server.on("/GBP", handle_GBP);
+  server.on("/EUR", handle_EUR);
+  server.on("/KWD", handle_KWD);
+  server.on("/INR", handle_INR);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+}
+
+void handle_OnConnect() {
+  server.send(200, "text/html", SendHTML("USD")); 
+}
+void handle_USD(){
+  client.publish("IOT_6B/G05/currency", "USD");
+  server.send(200, "text/html", SendHTML("USD"));
+}
+
+void handle_JPY(){
+  client.publish("IOT_6B/G05/currency", "JPY");
+  server.send(200, "text/html", SendHTML("JPY"));
+}
+
+void handle_GBP(){
+  client.publish("IOT_6B/G05/currency", "GBP");
+  server.send(200, "text/html", SendHTML("GBP"));
+}
+void handle_EUR(){
+  client.publish("IOT_6B/G05/currency", "EUR");
+  server.send(200, "text/html", SendHTML("EUR"));
+}
+void handle_KWD(){
+  client.publish("IOT_6B/G05/currency", "KWD");
+  server.send(200, "text/html", SendHTML("KWD"));
+}
+void handle_INR(){
+  client.publish("IOT_6B/G05/currency", "INR");
+  server.send(200, "text/html", SendHTML("INR"));
+}
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(String Currency){
+  String ptr = "<!DOCTYPE html> <html lang=\"en\">\n";
+  ptr+= "<head>\n";
+  ptr+= "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">\n";   
+  ptr+= "<meta charset=\"UTF-8\">\n";
+  ptr+= "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n";
+  ptr+= "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
+  ptr+= "<title>GROUP 5</title>\n";
+  ptr+= "</head>\n";
+  ptr+= "<body style=\"text-align:center;display:grid;place-content: center;background-color: rgb(23, 196, 196);\"\n";
+  ptr+= "<h1 \"style= font-size: 45px;\" >Get Exchange</h1>\n";
+  
+  if (Currency == "USD"){
+    ptr+="<h2>LKR/USD</h2>\n";
+  }
+  else if (Currency == "JPY"){
+    ptr+="<h2>LKR/JPY</h2>\n";
+  }
+  else if (Currency == "GBP"){
+    ptr+="<h2>LKR/GBP</h2>\n";
+  }
+  else if (Currency == "EUR"){
+    ptr+="<h2>LKR/EUR</h2>\n";
+  }
+  else if (Currency == "KWD"){
+    ptr+="<h2>LKR/KWD</h2>\n";
+  }
+  else if (Currency == "INR"){
+    ptr+="<h2>LKR/INR</h2>\n";
+  }
+  else{
+    ptr+="<h2>LKR/NON</h2>\n";
+  }
+  
+  ptr+= "<h1 style=\" color :rgb(62, 128, 0)\">1.81 <i class=\"fa fa-arrow-up\"></i></h1>\n";
+  ptr+= "<h1 style=\" color :red\">1.81 <i class=\"fa fa-arrow-down\"></i></h1>\n";
+//  ptr+= "<script>\n";
+//  ptr+= "function goToNewPage() {\n";
+//  ptr+= "if(document.getElementById('currency').value){\n";
+//  ptr+= "window.location.href = document.getElementById('currency').value;\n";
+//  ptr+= "}\n";
+//  ptr+= "}\n";
+//  ptr+= "</script>\n";
+  ptr+= "<form name=\"dropdown\" method=\"get\" style=\" font-size: xx-large;\" >\n";
+  ptr+= "<label for=\"currency_label\">Select Currency :</label><br>\n";
+  ptr+= "<select name=\"currency\" id=\"currency\">\n";
+  ptr+= "<option value=\"USD\">USD</option>\n";
+  ptr+= "<option value=\"JPY\">JPY</option>\n";
+  ptr+= "<option value=\"GBP\">GBP</option>\n";
+  ptr+= "<option value=\"EUR\">EUR</option>\n";
+  ptr+= "<option value=\"KWD\">KWD</option>\n";
+  ptr+= "<option value=\"INR\">INR</option>\n";
+  ptr+= "</select>\n";
+  ptr+= "<br><br>\n";
+  ptr+= "<label for=\"ceil\">Ceil% :</label><br>\n";
+  ptr+= "<input type=\"number\" id=\"ceil\" name=\"ceil\" value=5><br><br>\n";
+  ptr+= "<label for=\"floor\">Floor% :</label><br>\n";
+  ptr+= "<input type=\"number\" id=\"floor\" name=\"floor\" value=5><br><br>\n";
+  ptr+= "<input type=\"submit\" value=\"Submit\">\n";
+//  ptr+= "<input type=\"button\" value=\"Submit\" onclick=\"goToNewPage(document.dropdown.currency)\">\n";
+  ptr+= "</form>\n";
+  ptr+= "</body>\n";
+  ptr+= "</html>\n";
+  return ptr;
+}
